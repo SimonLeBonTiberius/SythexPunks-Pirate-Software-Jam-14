b137d02615d16648f95c9ff6b2734c3bdeab24ad
tibertisimone94@gmail.com <tibertisimone94@gmail.com>
2024-01-19 19:28:45 +0100

    adding count  spawn seed

--- scipt/SeedsDispenser.gd
+++ scipt/SeedsDispenser.gd
@@ -1,5 +1,7 @@
 extends Node
 @export var labelNode :Node
+@export var seedSpawn :Node
+var seed = preload("res://prefab/seed.tscn")
 
 # Called when the node enters the scene tree for the first time.
 #func _ready():
@@ -14,3 +16,10 @@ func hideInteractionButton(body):
 func showInteractionButton(body):
 	labelNode.show()
 
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_F && seedSpawn.get_child_count() < 3:
+			var instance_seed = seed.instantiate() 
+			seedSpawn.add_child(instance_seed)
+		if event.keycode == KEY_E && seedSpawn.get_child_count() > 0:
+			seedSpawn.get_children()[-1].free() 
